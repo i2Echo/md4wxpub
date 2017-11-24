40d04c9472d31a734ba39c180b5285ce34763799
echo <lzq.zheng@qq.com>
2017-11-24 22:37:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 require("./index.scss");
-// var $ = require("jquery");
+var $ = require("jquery");
 var Zepto = require("zepto");
+
+var _save = function(data){
+  localStorage.setItem("autosave", JSON.stringify(data));
+}
+
+var _read = function(){
+  return localStorage.getItem("autosave");
+}
+
+var data = {
+  content: null,
+  date: null
+}
+
+$('document').ready(function () {
+  if(_read()){
+      $("#test-editormd > textarea").value = JSON.parse(_read()).content;
+  }
+});
+
+setInterval(function(){
+  data.content = $("#test-editormd > textarea");
+  data.date = new Date.now();
+  _save(data);
+}, 60*1000);
